14b8988f3a8c3eeaec5ca21361a171333fbaa6ab
Dharun P <pdharun25@gmail.com>
2022-05-15 13:10:38 +0530

    moment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
 import Weather from './components/weather';
+import { Dimmer, Loader } from 'semantic-ui-react';
+
 export default function App() {
+
+
   
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
